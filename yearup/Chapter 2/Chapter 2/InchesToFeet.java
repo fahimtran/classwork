4978be8d61253304ea3c3037bb063e305025c456
fahimtran <46301587+fahimtran@users.noreply.github.com>
2019-04-15 15:18:15 -0400

    Cleaned up Folders 📁

--- yearup/Chapter 2/Chapter 2/InchesToFeet.java
+++
@@ -1,8 +0,0 @@
-
-public class InchesToFeet {
-	public static void main(String[] args) {
-		int inches = 32;
-		final int INCHES_IN_FEET = 12;
-		System.out.println( (inches/INCHES_IN_FEET)+ " feet and " + (inches % INCHES_IN_FEET) + " inches.");
-	}
-}
